053baf170ed2fcd705541c7e90e2cdde43c0b64c
jinook <cowlsdnr77@gmail.com>
2021-06-10 20:14:39 +0900

    week01 jinook commit

---
+++ programmers/해시/[42578_해시_위장]채진욱.py
@@ -0,0 +1,9 @@
+# 조합은 서로 다른 n개 중에 r개를 선택하는 경우의 수(순서 X)로 combinations 함수를 통해 이용할 수 있다.
+
+# import itertools
+
+# result = list(itertools.combinations((["1","2","3","4"]),2))
+# print("**경우의 수 : %s개" % len(result))
+# print(result)
+
+# dic['a'] = [1, 2, 3]

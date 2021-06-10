42f1815a4cc53d1e9865e51c6c28595042d32793
iamzin <eunzin.park@gmail.com>
2021-06-10 13:21:49 +0900

    2021.06.10 thu 42584, 42586, 12911

---
+++ programmers/42584_스택큐.py
@@ -0,0 +1,18 @@
+# queue: FIFO, [2, 3, 5]의 형태
+
+from collections import deque
+def solution(prices):
+    answer = []
+    
+    prices = deque(prices)
+    while prices:
+        cnt = 0
+        price = prices.popleft()
+        
+        for i in prices:
+            cnt += 1
+            if price > i:
+                break
+                
+        answer.append(cnt)
+    return answer

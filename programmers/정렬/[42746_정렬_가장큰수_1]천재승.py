aaabf836232bd70dba30292ecf1fa7d00aa35e8e
lovelyAlien <54395509+lovelyAlien@users.noreply.github.com>
2021-06-30 19:56:26 +0900

    solve algorithm_week05

--- programmers/정렬/[42746_정렬_가장큰수_1]천재승.py
+++ programmers/정렬/[42746_정렬_가장큰수_1]천재승.py
@@ -1,6 +1,7 @@
 
 # python2.X까지만 cmp 사용 가능
 # python3.X부터 cmp 매개변수 제거, 대신 cmp_to_key()함수가 표준 라이브러리의 functools 모듈에 추가 
+
 # 출처: https://comdoc.tistory.com/entry/7-가장-큰-수
 # 출처: https://docs.python.org/ko/3/howto/sorting.html
 
@@ -9,7 +10,7 @@ from functools import cmp_to_key
 def solution(numbers):
     answer = ''
     numbers_str=list(map(str, numbers))
-    
+    # 첫 번째가 두 번째보다 작으면(less-than) 음수 값을 반환하고, 같으면 0을 반환하고, 크면(greater-than) 양수 값을 반환해야 합니다.
     def compare(x, y): 
         if x+y>y+x:
             return -1

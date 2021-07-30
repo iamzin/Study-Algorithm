c355ece77247b234cf23faff3ae734eda68d9184
lovelyAlien <54395509+lovelyAlien@users.noreply.github.com>
2021-07-30 17:47:19 +0900

    feat: solve baekjoon problem 2178

--- java/src/baekjoon/search/problem2178/choun/Main.java
+++ java/src/baekjoon/search/problem2178/choun/Main.java
@@ -1,51 +1,58 @@
 package baekjoon.search.problem2178.choun;
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
+
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] input = br.readLine().split(" ");
-        int n = Integer.parseInt(input[0]);
-        int m = Integer.parseInt(input[1]);
-        int[][] maze = new int[n][m];
-        
-        for (int i=0; i<n; i++) {
-            String s = br.readLine();
 
-            for (int j=0; j<m; j++) {
-                maze[i][j] = s.charAt(j) - '0';
+
+        int row= Integer.parseInt(input[0]);
+        int col= Integer.parseInt(input[1]);
+
+        int[][] graph=new int[row][col];
+
+        for (int i=0; i<row; i++){
+            String s=br.readLine();
+
+            for( int j=0; j<col; j++){
+                graph[i][j]=s.charAt(j)-'0';
+
             }
         }
 
-        int result = bfs(n, m, maze);
-        System.out.println(result);
+        System.out.println(bfs(row,col,graph));
     }
 
-    static int bfs(int n, int m, int[][] maze) {
-        int[] dx = {0, 0, -1, 1};
-        int[] dy = {1, -1, 0, 0};
 
-        Queue<Dot> q = new LinkedList<>();
-        maze[0][0] = 0;
-        q.add(new Dot(0, 0, 1));
-        
-        while (!q.isEmpty()) {
-            Dot dot = q.poll();
-            
-            if (dot.x == n-1 && dot.y == m-1) {
-                return dot.count;
+    static int bfs(int row, int col, int [][]visited){
+        int[] drow={-1,1,0,0};
+        int[] dcol={0,0,-1,1};
+
+        Queue<Node> queue=new LinkedList<>();
+
+        visited[0][0]=0;
+        queue.add(new Node(0,0,1));
+
+
+        while (!queue.isEmpty()){
+            Node cur=queue.poll();
+            if(cur.row==row-1 && cur.col==col-1){
+                return cur.cnt;
             }
+            for (int i=0; i<4;i++){
 
-            for (int i=0; i<4; i++) {
-                int nx = dot.x + dx[i];
-                int ny = dot.y + dy[i];
-
-                if (0 <= nx && nx < n && 0 <= ny && ny < m) {
-                    if (maze[nx][ny] == 1) {
-                        maze[nx][ny] = 0;
-                        q.add(new Dot(nx, ny, dot.count + 1));
-                    }
+                int nr=cur.row+drow[i];
+                int nc=cur.col+dcol[i];
+                int cnt=cur.cnt;
+                if(nr>=0 && nr<row && nc>=0 && nc<col && visited[nr][nc]==1){
+                    visited[nr][nc]=0;
+                    queue.add(new Node(nr,nc,cnt+1));
                 }
             }
         }
@@ -53,15 +60,13 @@ public class Main {
         return 0;
     }
 
-    static class Dot {
-        int x, y, count;
-
-        Dot(int x, int y, int count) {
-            this.x = x;
-            this.y = y;
-            this.count = count;
+    class Node{
+        int row, col, cnt;
+        Node(int row, int col ,int cnt){
+            this.row=row;
+            this.col=col;
+            this.cnt=cnt;
         }
     }
-
-
 }
+
